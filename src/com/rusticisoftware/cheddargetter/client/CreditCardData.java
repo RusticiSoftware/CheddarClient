c725ea983cea76fcea8e06f0c695025bc737e04c
David Ells <david.ells@scorm.com>
2010-03-06 07:09:57 +0000

    Use struts UI tags for change account, clean up validation and related, add some new style rules for the struts output
    git-svn-id: https://svn.rusticisoftware.com/svn/projects/Products/IKI/SCORMCOM/trunk/CheddarClient@11687 ad08c97e-9a45-6745-9bb2-ebd1718b8280

--- src/com/rusticisoftware/cheddargetter/client/CreditCardData.java
+++ src/com/rusticisoftware/cheddargetter/client/CreditCardData.java
@@ -11,27 +11,55 @@ public class CreditCardData {
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getType() {
 		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getLastFour() {
 		return lastFour;
 	}
 
+	public void setLastFour(String lastFour) {
+		this.lastFour = lastFour;
+	}
+
 	public int getExpireMonth() {
 		return expireMonth;
+	}
+
+	public void setExpireMonth(int expireMonth) {
+		this.expireMonth = expireMonth;
 	}
 
 	public int getExpireYear() {
 		return expireYear;
 	}
 
+	public void setExpireYear(int expireYear) {
+		this.expireYear = expireYear;
+	}
+
+	public CreditCardData() {
+		
+	}
+	
 	public CreditCardData(String firstName, String lastName, String type, String lastFour, int expireMonth, int expireYear){
 		this.firstName = firstName;
 		this.lastName = lastName;
