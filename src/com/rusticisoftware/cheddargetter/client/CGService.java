20f90e6d6156a835d1a39cbb4577b22582910801
David Ells <david.ells@scorm.com>
2011-04-25 13:32:21 -0500

    Add license text

--- src/com/rusticisoftware/cheddargetter/client/CGService.java
+++ src/com/rusticisoftware/cheddargetter/client/CGService.java
@@ -1,3 +1,31 @@
+/* Software License Agreement (BSD License)
+ * 
+ * Copyright (c) 2010-2011, Rustici Software, LLC
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Rustici Software, LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.rusticisoftware.cheddargetter.client;
 
 import java.io.BufferedReader;
@@ -26,11 +54,6 @@ import sun.misc.BASE64Encoder;
 
 public class CGService implements ICGService {
 	private static Logger log = Logger.getLogger(CGService.class.toString());
-	
-	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-	static {
-		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-	}
 	
 	private static String CG_SERVICE_ROOT = "https://cheddargetter.com/xml";
 	
@@ -434,6 +457,8 @@ public class CGService implements ICGService {
 		}
 		
 		try{
+	        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+		    sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
 			return sdf.parse(fixDateFormat(cgDate));
 		}
 		catch (Exception e){
