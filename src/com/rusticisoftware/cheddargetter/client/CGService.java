3f3e4c1415adadfee4e09addd0c782a5070bf1e5
David Ells <david.ells@scorm.com>
2012-01-12 09:13:36 -0600

    Use a more reasonable timeout here

--- src/com/rusticisoftware/cheddargetter/client/CGService.java
+++ src/com/rusticisoftware/cheddargetter/client/CGService.java
@@ -386,8 +386,8 @@ public class CGService implements ICGService {
 		//Create a new request to send this data...
 		URL url = new URL(urlStr);
 		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-		connection.setConnectTimeout(1000);
-		connection.setReadTimeout(1000);
+		connection.setConnectTimeout(10000);
+		connection.setReadTimeout(10000);
 		InputStream inputStream = null;
 		InputStream errorStream = null;
 		OutputStream outputStream = null;
